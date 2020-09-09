fd76719374e7f47cc5aeb9ee164215866c166d54
Yogesh Singh <59567592+yogesh-singh028@users.noreply.github.com>
2020-09-09 20:16:33 +0530

    Add files via upload

--- SearchApp.py
+++ SearchApp.py
@@ -1,79 +1,88 @@
-
-# import modules ..
+# Icons made by <a href="https://www.flaticon.com/free-icon/search_639426" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 import wikipedia as wiki
 from tkinter.messagebox import askokcancel
 import threading
-
-# main gui ..
-root = Tk()
+# function ...
 
 def search():
+    global lang_dict
     search_data = ent.get()
+    val = lang.get()
+    text.delete(0.0, END)
+    text.insert(END,'Searching for {}'.format(search_data))
     try:
-        data = wiki.summary(search_data,sentences=5)
+        wiki.set_lang(lang_dict[val])
+        data = wiki.summary(search_data, sentences=8)
     except Exception as e:
         data = e
 
-    # delete data .
+    # delete current data ..
     ent.set('')
     text.delete(0.0,END)
-
-    search_lbl['text'] = 'Searching For : {}'.format(search_data)
-    text.insert(END,data)
+    # insert data
+    search_lbl['text'] = "Searching result for :{}".format(search_data)
+    text.insert(0.0,data)
 
 def call_search(*args):
     x = threading.Thread(target=search)
     x.start()
 
-def call_back(*args):
-    if askokcancel('Quit','Are you sure'):
+def callbackforroot():
+    if askokcancel('Quit','Do you really want to quit?'):
         root.quit()
 
+def copy_data(event):
+    data = text.get(0.0, END)
+    root.clipboard_clear()  # clear clipboard contents
+    root.clipboard_append(data)  # append new value to clipbaord
 
 
-root.title("Search App")
+
+
+
+
+root = Tk()
+root.title('Search Application')
 root.geometry('320x480')
+root.protocol("WM_DELETE_WINDOW",callbackforroot)
 root.configure(bg='white')
-root.protocol('WM_DELETE_WINDOW',call_back)
 
-
-# adding widgets ..
+# test variable
 ent = StringVar()
-search_entry = Entry(root,width=30,font=('arial',12),bd=2,relief=RIDGE,textvariable=ent)
+lang = StringVar()
+
+lang_dict = {'English':'en','Hindi':'hi','Gujrati':'gu','French':'fr','German':'de','Arabic':'ar'}
+
+search_entry = Entry(root,width=21,font=('arial',14),bd=2,relief=RIDGE,textvariable=ent)
 search_entry.bind('<Return>',call_search)
 search_entry.place(x=15,y=20)
 
-search_lbl = Label(root,text="Searching For : ",font=('arial',12,'bold'),bg='white')
+img = PhotoImage(file='search.png')
+
+search_button =Button(root,image=img,bd=2,relief=GROOVE,command=call_search)
+search_button.place(x=250,y=20)
+
+search_lbl = Label(root,text='Searching result for :  ',font=('arial',12,'bold'),bg='white')
 search_lbl.place(x=15,y=70)
 
 text = ScrolledText(root,font=('times',12),bd=4,relief=SUNKEN,wrap =WORD)
-text.place(x=15,y=100,width=300,height=300)
+text.bind('<Double-1>',copy_data)
+text.place(x=15,y=100,height=300,width=300)
 
+lang_list= ['English','Hindi','Gujrati','French','German','Arabic']
+lang.set(lang_list[0])
 
-# Button ...
+language = OptionMenu(root,lang,*lang_list)
+language.place(x=10,y=420)
 
-search_btn = Button(root,text='Search',font=('arial',12,'bold'),width=10,command=search)
-search_btn.place(x=10,y=420)
+clear_btn = Button(root,text='Clear',font=('arial',10,'bold'),width=10,
+                   command=lambda :text.delete(0.0,END))
+clear_btn.place(x=100,y=420)
 
-clear_btn = Button(root,text='Clear',font=('arial',12,'bold'),width=10,command=lambda :text.delete(0.0,END))
-clear_btn.place(x=105,y=420)
-
-exit_btn = Button(root,text='Exit',font=('arial',12,'bold'),width=10,command=root.quit)
-exit_btn.place(x=200,y=420)
-
-
-
-
-
-
-
-
-
-
-
-
+exit_btn = Button(root,text='Exit',font=('arial',10,'bold'),width=10,command=root.quit)
+exit_btn.place(x=210,y=420)
 
 
 root.mainloop()
